b46ed8a91ac3a8ac3554220ab4c49510fba2bb1f
atidele <alejandro.tidele@mercadolibre.com>
2020-09-10 11:25:59 -0300

    Starting! 🚀

---
+++ app/src/main/java/src/Activities/MainActivity.java
@@ -0,0 +1,25 @@
+package src.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tp3_pa_grupo_3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLogin(View view) {
+
+    }
+
+    public void onRegister(View view) {
+
+    }
+}
